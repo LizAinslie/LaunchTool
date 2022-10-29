7d616beb32d55ce168c37624cd372e7614b6e1fc
Liz Ainslie <lizainslie16@gmail.com>
2022-10-29 01:36:47 -0700

    chore(data): jvm - list instances

--- src/jvmMain/kotlin/lgbt/lizzy/launch/instance/InstanceManager.kt
+++ src/jvmMain/kotlin/lgbt/lizzy/launch/instance/InstanceManager.kt
@@ -1,9 +1,16 @@
 package lgbt.lizzy.launch.instance
 
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import lgbt.lizzy.launch.util.AppData
+import java.io.File
+import java.nio.charset.Charset
+
 actual object InstanceManager {
-    actual fun getAllInstances(): List<MinecraftInstance> {
-        TODO("Not yet implemented")
-    }
+    actual fun getAllInstances(): List<MinecraftInstance> =
+        File(AppData.instance.getAppData(), "instances").listFiles()?.map {
+            Json.decodeFromString(it.readText(Charset.defaultCharset()))
+        } ?: listOf()
 
     actual fun launchInstance(instance: MinecraftInstance) {
     }
